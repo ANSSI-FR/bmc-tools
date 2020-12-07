5f5aaa3974d38a19df66bddee7e9274fd35af55c
yam <19171514+nitrogenc@users.noreply.github.com>
2020-12-07 22:51:29 +0100

    Updated executable version.

--- bmc-tools.py
+++ bmc-tools.py
@@ -211,7 +211,7 @@ class BMCContainer():
 		return True
 
 if __name__ == "__main__":
-	prs = argparse.ArgumentParser(description="RDP Bitmap Cache parser (v. 2.00, 04/12/2020)")
+	prs = argparse.ArgumentParser(description="RDP Bitmap Cache parser (v. 2.01, 2020/12/07)")
 	prs.add_argument("-s", "--src", help="Specify the BMCache file or directory to process.", required=True)
 	prs.add_argument("-d", "--dest", help="Specify the directory where to store the extracted bitmaps.", required=True)
 	prs.add_argument("-c", "--count", help="Only extract the given number of bitmaps.", type=int, default=-1)
